8217b895ebea3c20f565598614d5fdaa6838d699
Noanb7 <noadelhaye7@gmail.com>
2025-09-18 15:22:03 +0200

    Modification de la fonction distance pour accepter des paramètres de temps et de vitesse

--- intro/TP/Tp exercice 5c.py
+++ intro/TP/Tp exercice 5c.py
@@ -1,16 +1,13 @@
-def distance(km):
+def distance(km, min, s):
 
     ml = km/1.61
 
-    min = 43
-    s = 30
-
-    s += 43 * 60
+    s += min * 60
 
     ml_h = ml/s
     ml_h = ml_h * 3600
 
     s_ml = s/ml
     return (s_ml)
-result = distance(10)
+result = distance(10, 43, 30)
 print(result)
