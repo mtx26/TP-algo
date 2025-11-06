1d331d5510c06ac0fdc751ca4c54ce12b6ea0dc6
mtx26 <mtx12345626@gmail.com>
2025-11-06 15:46:12 +0100

    Ajout de la fonction produit_matriciel pour effectuer la multiplication de matrices

---
+++ TP_6/ex_5.py
@@ -0,0 +1,53 @@
+def produit_matriciel(A, B):
+    m = 0
+    n = 0
+    n2 = 0
+    p = 0
+
+    # calculer m et n
+    for l in A:
+        m += 1
+    for c in A[0]:
+        n += 1
+
+    # calculer n et p
+    for l in B:
+        n2 += 1
+    for c in B[0]:
+        p += 1
+
+    if n != n2:
+        return None
+
+    # creation matrice rep :
+    i = 0
+    row = []
+    while i < m:
+        row2 = []
+        j = 0
+        while j < p:
+            row2.append(0)
+            j += 1
+        row.append(row2)
+        i += 1
+    print(row)
+            
+
+    i = 0
+    while i < m:
+        j = 0
+        while j < p:
+            k = 0
+            rep = 0
+            while k < n:
+                rep += A[i][k] * B[k][j]
+                print(A[i][k], B[k][j])
+                k += 1
+            print(rep)
+            row[i][j] = rep
+            j += 1
+        i += 1
+
+    return row
+
+print(produit_matriciel([[1, 5], [2, 6], [3, 7]], [[2, 0, 1], [3, 6, 2]]))
