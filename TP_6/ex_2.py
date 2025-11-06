4968981c0e1a179d1b570462975c6bb06d95391a
mtx26 <mtx12345626@gmail.com>
2025-11-06 14:40:01 +0100

    Ajout de la fonction prime_number pour générer et afficher les n premiers nombres premiers

---
+++ TP_6/ex_2.py
@@ -0,0 +1,28 @@
+import math
+
+def prime_number(n):
+    i = 0
+    rep = []
+    num = 2
+
+    while i < n:
+        len = math.ceil(math.sqrt(num))
+        j = 2
+        isprime = True
+        if num == len:
+            rep += [num]
+        while j <= len:
+            if num % j == 0:
+                isprime = False
+            j += 1
+        if isprime:
+            rep += [num]
+            i += 1
+            print(num)
+        num += 1
+    print(rep)
+
+prime_number(9)
+
+
+
