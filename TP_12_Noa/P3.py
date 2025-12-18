72138e561b3661d6dbbab7ffa5b3a54f2201d3f9
Noanb7 <noadelhaye7@gmail.com>
2025-12-18 14:10:51 +0100

    Ajout de la classe Personne avec gestion des amis et validation des entrées, ainsi que la classe Reseau avec vérification des amis narcissiques.

--- TP_12_Noa/P3.py
+++ TP_12_Noa/P3.py
@@ -1,3 +1,38 @@
+class Personne():
+
+    def __init__(self, name, email, friends):
+
+        if not isinstance(name, str): 
+            raise ValueError("Attention, le nom de la personne doit etre une chaine de caractère !")
+        if not isinstance(email, str): 
+            raise ValueError("Attention, l'email de la personne doit etre une chaine de caractère !")
+        if not isinstance(friends, list): 
+            raise ValueError("Attention, la liste d'amis de la personne doit etre une liste python !")
+            
+
+        self.name = name
+        self.email = email
+        self.friends = []
+
+    def ajouter_un_ami(self, ami):
+        pass
+
+    def ajouter_les_amis_d_amis(self):
+
+        pass
+        
+    
+    def __str__(self):
+        return "Une personne, sont nom est " + self.name + "son adresse email est " + self.email + "ses amis sont " + self.friends
+
+
+class Reseau(Personne):
+    def __init__(self, liste):
+        if not isinstance(liste, list): 
+            raise ValueError("Attention, la liste d'amis de la personne doit etre une liste python !")
+        
+    def non_narcissique(self):
+        pass
 
 if __name__== "__main__":
     pass
