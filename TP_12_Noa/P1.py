4450640414c98adcf9e24de7d7f50005736de8fa
Noanb7 <noadelhaye7@gmail.com>
2025-12-18 13:45:30 +0100

    Ajout des fichiers P1.py, P2.py, P3.py et P4.py avec des fonctions de base pour le traitement des pièces et le calcul de l'aire.

---
+++ TP_12_Noa/P1.py
@@ -0,0 +1,11 @@
+def peut_aller(piece1, piece2, portes, eviter):
+    pass
+
+def chemin(piece1, piece2, portes, eviter):
+    pass
+
+def plus_court_chemin(piece1, piece2, portes, eviter):
+    pass
+
+if __name__== "__main__":
+    pass
