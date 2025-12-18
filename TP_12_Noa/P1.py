263ab66d326c06c2565c388dbc1696a084d2d726
Noanb7 <noadelhaye7@gmail.com>
2025-12-18 14:12:49 +0100

    Implémentation de la fonction peut_aller pour vérifier si un chemin existe entre deux pièces en utilisant les portes disponibles.

--- TP_12_Noa/P1.py
+++ TP_12_Noa/P1.py
@@ -1,5 +1,11 @@
 def peut_aller(piece1, piece2, portes, eviter):
-    pass
+        if piece1 == piece2:
+            return True
+    
+        for (p_dep, p_arr) in portes:
+            if p_dep == piece1 and p_arr not in eviter:
+                if peut_aller(p_arr, piece2, portes):
+                    return True
 
 def chemin(piece1, piece2, portes, eviter):
     pass
