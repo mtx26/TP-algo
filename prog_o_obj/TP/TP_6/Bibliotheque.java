38b8ae3718f8db20d1f15ed68b784597d2023b99
mtx26 <mtx12345626@gmail.com>
2026-05-08 09:29:50 +0200

    send

--- prog_o_obj/TP/TP_6/Bibliotheque.java
+++
@@ -1,7 +0,0 @@
-package TP.TP_6;
-
-public abstract class Bibliotheque {
-    public abstract void ajouter(Ouvrage o);
-    public abstract void supprimer(int cote);
-    public abstract String toString();
-}
